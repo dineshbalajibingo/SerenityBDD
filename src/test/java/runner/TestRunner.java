646607be8bc14cdf0f37269e1aab270634d22bcb
dinesven <dineshbalajibingo@gmail.com>
2021-07-25 19:24:20 +0530

    Final Change for the code Run as JAR

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -13,21 +13,6 @@ import org.junit.runner.RunWith;
         glue = {"steps"}
 )
 public class TestRunner {
-   /* private static String[] defaultOptions = {
-            "--glue", "steps",
-            "--features", "src/test/resources/features",
-            "--plugin", "pretty",
 
-    };
-    public static void main(String[] args) {
-        SecurityManager manager = new IgnoreExitCall();
-        System.setSecurityManager(manager);
-        try {
-            Stream<String> cucumberOptions = Stream.concat(Stream.of(defaultOptions), Stream.of(args));
-            io.cucumber.core.cli.Main.main(cucumberOptions.toArray(String[]::new));
-        } catch (SecurityException e) {
-            System.out.println("Ignore exit");
-        }
 
-    }*/
 }
